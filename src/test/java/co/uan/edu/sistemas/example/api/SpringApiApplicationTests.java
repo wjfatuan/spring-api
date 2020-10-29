75477b2db46faca1d9be052ee60efc533730eb43
wjfatuan <wilson.forero@uan.edu.co>
2020-10-29 18:13:50 -0500

    Initial setup

---
+++ src/test/java/co/uan/edu/sistemas/example/api/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uan.edu.sistemas.example.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
