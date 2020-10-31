06cdb39ed99e3db2aebe37c4e58aa79ee66e07d5
wjfatuan <wilson.forero@uan.edu.co>
2020-10-31 12:26:31 -0500

    cors

--- src/main/java/co/uan/edu/sistemas/example/api/DataRepository.java
+++ src/main/java/co/uan/edu/sistemas/example/api/DataRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "data", path = "data")
 public interface DataRepository extends CrudRepository<Data, Long> {
 
