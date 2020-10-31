35b13da394e47172d327c29768dcf4afc7beb6ff
wjfatuan <wilson.forero@uan.edu.co>
2020-10-31 17:05:13 -0500

    unit tests

--- src/main/java/co/uan/edu/sistemas/example/api/DatabaseConfig.java
+++ src/main/java/co/uan/edu/sistemas/example/api/DatabaseConfig.java
@@ -1,7 +1,9 @@
 package co.uan.edu.sistemas.example.api;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
