6b29315fac1945d2c48b479594c2e858cefad8de
wjfatuan <wilson.forero@uan.edu.co>
2020-10-31 13:49:51 -0500

    adding postgres profile (heroku)

--- src/main/java/co/uan/edu/sistemas/example/api/DatabaseConfig.java
+++ src/main/java/co/uan/edu/sistemas/example/api/DatabaseConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
+@Profile("heroku")
 @Configuration
 public class DatabaseConfig {
 
