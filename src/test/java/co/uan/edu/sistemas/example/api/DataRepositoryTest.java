60ce104fdf2bb460fea85a586fe9fe61b410b293
wjfatuan <wilson.forero@uan.edu.co>
2020-10-31 16:25:48 -0500

    initial testing setup

--- src/test/java/co/uan/edu/sistemas/example/api/DataRepositoryTest.java
+++ src/test/java/co/uan/edu/sistemas/example/api/DataRepositoryTest.java
@@ -5,16 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+// import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
 public class DataRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+    // @Autowired
+    // private TestEntityManager entityManager;
 
     @Autowired
-    DataRepository repository;
+    private DataRepository repository;
 
     @Test
     public void emptyRepository() {
